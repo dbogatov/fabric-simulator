921b3a4971da8046356eb5de4ed7f61fddc9e31d
Dmytro Bogatov <dmytro@dbogatov.org>
2020-02-01 20:20:39 -0500

    Fix plots.

--- plots/usage.py
+++ plots/usage.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 from bokeh.plotting import figure, output_file, show
+from bokeh.io import export_svgs
+from bokeh.models import DatetimeTickFormatter
+
 import pandas as pd
 from datetime import datetime as dt
 import json
@@ -11,19 +14,25 @@ with open("../usage.json") as f:
 x = list(map(lambda point: pd.to_datetime(point["X"]), data))
 y = list(map(lambda point: point["Y"], data))
 
-output_file("line.html")
-
-p = figure(
+plot = figure(
 	x_axis_type="datetime",
 	plot_width=1500,
 	plot_height=300
 )
 
+formatterArgs = {}
+for property in ["months", "days", "hours", "hourmin", "minutes", "minsec", "seconds", "milliseconds"]:
+	formatterArgs[property] = ["%H:%M:%S.%3Ns"]
+plot.xaxis.formatter = DatetimeTickFormatter(**formatterArgs)
+
 # add a line renderer
-p.line(
+plot.line(
 	x=x,
 	y=y,
 	line_width=2
 )
 
-show(p)
+# plot.output_backend = "svg"
+# export_svgs(plot, filename="plot.svg")
+
+show(plot)
